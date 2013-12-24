b711d42fb81e368cd367d648da71935195107919
mikhaylova-daria <dasham94@yandex.ru>
2013-12-25 03:03:46 +0400

    поправлен баг в построении автомата, ускорен поиск good_link

--- Aho_Corasick/AhoCorasick.h
+++ Aho_Corasick/AhoCorasick.h
@@ -9,7 +9,7 @@ class AhoCorasick {
     struct Node {
         std::unordered_map<char, std::shared_ptr<Node> > sons;
         std::vector<int> pos_pattern_in_dictionary;
-        bool is_finish;
+        bool is_finish = false;
         std::weak_ptr<Node> suf_link;
         std::weak_ptr<Node> good_link;
         std::weak_ptr<Node> parent;
@@ -25,10 +25,13 @@ class AhoCorasick {
 public:
     AhoCorasick(std::vector<std::string> _dictionary): dictionary(_dictionary) {
         building_trie();
+        suf_linking();
     }
 
     void building_trie() {
-        root = std::shared_ptr<Node>(new Node());
+        root = std::shared_ptr<Node>(new Node(root, '/0'));
+        root->is_finish = true;
+        root->suf_link = root;
         std::unordered_map<char, std::shared_ptr<Node> >::iterator itr;
         std::weak_ptr<Node> node = root;
         for (int i = 0; i < dictionary.size(); ++i) {
@@ -55,9 +58,10 @@ public:
             gray.pop_front();
             std::unordered_map<char, std::shared_ptr<Node> >:: iterator itr_child;
             for (itr_child = node.lock()->sons.begin(); itr_child != node.lock()->sons.end(); ++itr_child) {
+
                 itr_child->second->suf_link = jump(itr_child->second->parent.lock()->suf_link, itr_child->second->term);
                 itr_child->second->good_link = good_jump(itr_child->second);
-                gray.push_back(node);
+                gray.push_back(itr_child->second);
             }
         }
     }
@@ -66,15 +70,19 @@ public:
         if (node.lock()->term == c) {
             return node;
         } else {
+            if (node.lock() == root) {
+                return root;
+            }
             jump(node.lock()->parent, c);
         }
     }
 
     std::weak_ptr<Node> good_jump(std::weak_ptr<Node> node) {
-        if (node.lock()->is_finish) {
+        if (node.lock()->suf_link.lock()->is_finish) {
             return node.lock()->suf_link;
         } else {
-            good_jump(node.lock()->suf_link);
+            return node.lock()->good_link;
+           // good_jump(node.lock()->suf_link);
         }
     }
 };
