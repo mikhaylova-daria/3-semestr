7d2db721b24ada533790f4346cd5089260358256
mikhaylova-daria <dasham94@yandex.ru>
2013-12-25 11:04:35 +0400

    поправлены найденные баги. осуществляет поиск

--- Aho_Corasick/AhoCorasick.h
+++ Aho_Corasick/AhoCorasick.h
@@ -8,7 +8,7 @@
 class AhoCorasick {
     struct Node {
         std::unordered_map<char, std::shared_ptr<Node> > sons;
-        std::vector<int> pos_pattern_in_dictionary;
+        int pos_pattern_in_dictionary = -1;
         bool is_finish = false;
         std::weak_ptr<Node> suf_link;
         std::weak_ptr<Node> good_link;
@@ -21,7 +21,8 @@ class AhoCorasick {
 
     std::vector<std::string> dictionary;
     std::shared_ptr<Node> root;
-
+    int pos_in_text = 0;
+    std::vector<std::pair <std::string, unsigned long long> > answer;
 public:
     AhoCorasick(std::vector<std::string> _dictionary): dictionary(_dictionary) {
         building_trie();
@@ -45,7 +46,7 @@ public:
                 node = itr -> second;
             }
             node.lock()->is_finish = true;
-            node.lock()->pos_pattern_in_dictionary.push_back(i);
+            node.lock()->pos_pattern_in_dictionary = i;
             node = root;
         }
     }
@@ -57,33 +58,97 @@ public:
             std::weak_ptr<Node> node = gray.front();
             gray.pop_front();
             std::unordered_map<char, std::shared_ptr<Node> >:: iterator itr_child;
-            for (itr_child = node.lock()->sons.begin(); itr_child != node.lock()->sons.end(); ++itr_child) {
+            std::cout << node.lock()->term<< ":" <<std::endl;
 
-                itr_child->second->suf_link = jump(itr_child->second->parent.lock()->suf_link, itr_child->second->term);
-                itr_child->second->good_link = good_jump(itr_child->second);
+            for (itr_child = node.lock()->sons.begin(); itr_child != node.lock()->sons.end(); ++itr_child) {
+                if (itr_child->second->parent.lock() == root) {
+                    itr_child->second->suf_link = root;
+                    itr_child->second->good_link = root;
+                } else {
+                    itr_child->second->suf_link = jump(itr_child->second->parent.lock()->suf_link, itr_child->second->term);
+                    itr_child->second->good_link = good_jump(itr_child->second);
+                }
+                std::cout<<itr_child->second->term<<" "<<itr_child->second->parent.lock()->term;
+                std::cout<<"("<<itr_child->second->suf_link.lock()->term<<")";
+                std::cout<<"<"<<itr_child->second->good_link.lock()->term<<">";
+std::cout<<std::endl;
                 gray.push_back(itr_child->second);
             }
         }
+
     }
 
     std::weak_ptr<Node> jump(std::weak_ptr<Node> node, char c) {
-        if (node.lock()->term == c) {
-            return node;
+        std::unordered_map<char, std::shared_ptr<Node> > ::iterator itr;
+        itr = node.lock()->sons.find(c);
+        if (itr != node.lock()->sons.end()) {
+            std::cout<<"*"<<std::endl;
+            return itr->second;
         } else {
             if (node.lock() == root) {
                 return root;
             }
-            jump(node.lock()->parent, c);
+            return jump(node.lock()->suf_link, c);
+            std::cout<<"@"<<std::endl;
         }
     }
 
     std::weak_ptr<Node> good_jump(std::weak_ptr<Node> node) {
         if (node.lock()->suf_link.lock()->is_finish) {
+           // std::cout<<"yes"<<std::endl;
             return node.lock()->suf_link;
         } else {
-            return node.lock()->good_link;
-           // good_jump(node.lock()->suf_link);
+           // return node.lock()->suf_link.lock()->good_link;
+            return good_jump(node.lock()->suf_link);
         }
     }
+
+    std::vector<std::pair <std::string, unsigned long long> > search(std::istream& istr) {
+        answer.clear();
+        std::weak_ptr<Node> node = root;
+        char current_char;
+        istr >> current_char;
+        ++pos_in_text;
+        std::unordered_map<char, std::shared_ptr<Node> >::iterator itr;
+        while ((!istr.eof())) {
+            itr = node.lock()->sons.find(current_char);
+            while (itr == node.lock()->sons.end()) {
+                node = node.lock()->suf_link;
+                //write(node);
+                itr = node.lock()->sons.find(current_char);
+//                std::cout<<"!!"<<std::endl;
+                if ((node.lock() == root)
+                        && (itr == node.lock()->sons.end())) {
+                    istr >> current_char;
+                    ++pos_in_text;
+                }
+            }
+            node = itr->second;
+            istr >> current_char;
+            ++pos_in_text;
+            write(node);
+        }
+        return answer;
+    }
+
+    void write(std::weak_ptr<Node> node) {
+        if (node.lock()->is_finish) {
+            answer.push_back(std::pair <std::string, unsigned long long>
+                         (dictionary[node.lock()->pos_pattern_in_dictionary],
+                         pos_in_text - dictionary[node.lock()->pos_pattern_in_dictionary].size()));
+            std::cout<<"new"<<node.lock()->pos_pattern_in_dictionary<<std::endl;
+        }
+            while (node.lock() != root) {
+                node = node.lock()->good_link;
+                if (node.lock()->pos_pattern_in_dictionary > -1) {
+                    answer.push_back(std::pair <std::string, unsigned long long>
+                                    (dictionary[node.lock()->pos_pattern_in_dictionary],
+                                    pos_in_text - dictionary[node.lock()->pos_pattern_in_dictionary].size()));
+                }
+            }
+
+    }
+
+
 };
 #endif // AHOCORASICK_H
--- Aho_Corasick/main.cpp
+++ Aho_Corasick/main.cpp
@@ -1,16 +1,24 @@
 #include <iostream>
+#include <fstream>
 #include "AhoCorasick.h"
 using namespace std;
 
 int main()
 {
     std::vector<string> d;
-    while (cin) {
+    ifstream istr("/home/darya/OOP/Aho_Corasick/input.txt");
+    int n;
+    istr>>n;
+    for (int i = 0; i < n; ++i) {
         std::string str;
-        cin>>str;
+        istr>>str;
         d.push_back(str);
     }
     AhoCorasick ah(d);
+    std::vector<std::pair<std::string, unsigned long long> > answer = ah.search(istr);
+    for (int i = 0; i < answer.size(); ++i) {
+        cout<<answer[i].first<< " " << answer[i].second << std::endl;
+    }
     cout << "Hello World!" << endl;
     return 0;
 }
